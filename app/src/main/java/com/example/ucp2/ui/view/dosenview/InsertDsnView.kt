b162bb76c16031ee47a025e45c9947f20971ecb5
kaniadhea <kaniadhea7@gmail.com>
2024-12-21 21:56:38 +0700

    Fungsi FormDosen dibuat untuk memenuhi kebutuhan pengelolaan data dosen dalam aplikasi

--- app/src/main/java/com/example/ucp2/ui/view/dosenview/InsertDsnView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosenview/InsertDsnView.kt
@@ -1,2 +1,17 @@
 package com.example.ucp2.ui.view.dosenview
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.ucp2.ui.viewmodel.dosen.DosenEvent
+import com.example.ucp2.ui.viewmodel.dosen.FormErrorState
+
+
+@Composable
+fun FormDosen(
+    dosenEvent: DosenEvent = DosenEvent(),
+    onValueChange : (DosenEvent) -> Unit,
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier
+){
+
+}
