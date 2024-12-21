71e28200cf54fca1a1cc7f31f8e4e6820279d0d0
kaniadhea <kaniadhea7@gmail.com>
2024-12-22 00:17:37 +0700

    memastikan konten nya berada di tengah

--- app/src/main/java/com/example/ucp2/ui/costumwidget/CstAppBar.kt
+++ app/src/main/java/com/example/ucp2/ui/costumwidget/CstAppBar.kt
@@ -1,7 +1,16 @@
 package com.example.ucp2.ui.costumwidget
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CstAppBar(
@@ -10,5 +19,27 @@ fun CstAppBar(
     judul: String,
     modifier: Modifier = Modifier
 ){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ){
+        if (showBackButton) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TextButton(
+                    onClick = onBack,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                ) {
+                    Text("Kembali")
+                }
+                Spacer(modifier = Modifier.weight(2f))
+            }
+        }
 
+
+    }
 }
