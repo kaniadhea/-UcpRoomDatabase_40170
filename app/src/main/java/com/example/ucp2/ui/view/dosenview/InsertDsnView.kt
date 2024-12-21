23bb6461c7746ad7bb19047ed9bc81a875b38179
kaniadhea <kaniadhea7@gmail.com>
2024-12-22 00:23:27 +0700

    adalah untuk membuat tampilan formulir untuk menyisipkan data dosen

--- app/src/main/java/com/example/ucp2/ui/view/dosenview/InsertDsnView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosenview/InsertDsnView.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,9 +25,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.costumwidget.CstAppBar
 import com.example.ucp2.ui.viewmodel.PenyediaViewModel
 import com.example.ucp2.ui.viewmodel.dosen.DosenEvent
 import com.example.ucp2.ui.viewmodel.dosen.DosenViewModel
+import com.example.ucp2.ui.viewmodel.dosen.DsnUIState
 import com.example.ucp2.ui.viewmodel.dosen.FormErrorState
 import kotlinx.coroutines.launch
 
@@ -47,6 +53,39 @@ fun InserDsnView(
             }
         }
     }
+
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ){ padding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+
+        ){
+            CstAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Tambah Mahasiswa"
+            )
+
+
+        }
+
+    }
+}
+
+
+@Composable
+fun InsertBodyDsn(
+    modifier: Modifier= Modifier,
+    onValueChange: (DosenEvent) -> Unit,
+    uiState: DsnUIState,
+    onClick: () -> Unit
+){
+
 }
 
 
