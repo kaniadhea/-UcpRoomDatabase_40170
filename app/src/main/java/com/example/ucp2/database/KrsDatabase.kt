e511abb3d40d57544b09c61be4ea3ff25343edd3
kaniadhea <kaniadhea7@gmail.com>
2024-12-21 23:05:37 +0700

    Kode ini memastikan bahwa aplikasi memiliki akses ke Room Database

--- app/src/main/java/com/example/ucp2/database/KrsDatabase.kt
+++ app/src/main/java/com/example/ucp2/database/KrsDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.ucp2.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.ucp2.dao.dosendao.DosenDao
 import com.example.ucp2.entity.Dosen
@@ -10,6 +12,22 @@ abstract class KrsDatabase : RoomDatabase(){
 
     //Mendefinisikan fungsi untuk mengakses data Dosen
     abstract fun DosenDao(): DosenDao
+
+    companion object{
+        @Volatile
+        private var Instance: KrsDatabase? = null
+
+        fun getDatabase(context: Context): KrsDatabase{
+            return (Instance ?: synchronized(this){
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    KrsDatabase::class.java,
+                    "KrsDatabase"
+                )
+                    .build().also { Instance = it }
+            })
+        }
+    }
 
 
 }
