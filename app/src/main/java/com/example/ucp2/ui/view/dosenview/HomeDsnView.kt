339ad29746f6bf28f9cf86166d324a9a7017c45d
kaniadhea <kaniadhea7@gmail.com>
2024-12-22 01:15:11 +0700

    membuat card dosen di HomeDsnView.kt

---
+++ app/src/main/java/com/example/ucp2/ui/view/dosenview/HomeDsnView.kt
@@ -0,0 +1,15 @@
+package com.example.ucp2.ui.view.dosenview
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.ucp2.entity.Dosen
+
+@Composable
+
+fun CardDsn(
+    dsn: Dosen,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {}
+) {
+
+}
