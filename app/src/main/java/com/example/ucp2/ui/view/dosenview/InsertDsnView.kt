90a52d9f2bd64d36a93657f7fc8b0eaa10390a37
kaniadhea <kaniadhea7@gmail.com>
2024-12-21 22:09:28 +0700

    membuat input untuk nama, nidn dan jenis kelamin

--- app/src/main/java/com/example/ucp2/ui/view/dosenview/InsertDsnView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosenview/InsertDsnView.kt
@@ -1,11 +1,21 @@
 package com.example.ucp2.ui.view.dosenview
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import com.example.ucp2.ui.viewmodel.dosen.DosenEvent
 import com.example.ucp2.ui.viewmodel.dosen.FormErrorState
 
@@ -31,5 +41,46 @@ fun FormDosen(
             isError = errorState.nama != null,
             placeholder = { Text("Masukkan nama")}
         )
+        Text(
+            text = errorState.nama ?:"",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = dosenEvent.nidn, onValueChange = {
+                onValueChange(dosenEvent.copy(nidn = it))
+            },
+            label = { Text("NIDN")},
+            isError = errorState.nidn != null,
+            placeholder = { Text("Masukkan NIDN")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text(text = errorState.nidn ?: "", color = Color.Red)
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Jenis Kelamin")
+
+        Row (
+            modifier = Modifier.fillMaxWidth()
+        ){
+            jenisKelamin.forEach{jk ->
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ){
+                    RadioButton(
+                        selected = dosenEvent.jenisKelamin == jk,
+                        onClick = {
+                            onValueChange(dosenEvent.copy(jenisKelamin = jk ))
+                        },
+                    )
+                    Text(
+                        text = jk,
+                    )
+                }
+
+            }
+        }
     }
 }
