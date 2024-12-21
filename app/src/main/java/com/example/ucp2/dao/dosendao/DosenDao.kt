87c2481636993fa092899e3e2d953cabb2a5fb89
kaniadhea <kaniadhea7@gmail.com>
2024-12-21 23:40:58 +0700

    DosenDao adalah antarmuka untuk mengakses dan mengelola data dosen

--- app/src/main/java/com/example/ucp2/dao/dosendao/DosenDao.kt
+++ app/src/main/java/com/example/ucp2/dao/dosendao/DosenDao.kt
@@ -1,4 +1,18 @@
 package com.example.ucp2.dao.dosendao
 
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.ucp2.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
 interface DosenDao {
+
+    @Insert
+    suspend fun insertDosen(dosen: Dosen)
+
+    @Query("SELECT * FROM dosen ORDER BY nama ASC")
+    fun getAllDosen () : Flow<List<Dosen>>
+
+    @Query("SELECT * FROM dosen WHERE nidn = :nidn")
+    fun getMhs(nidn: String) : Flow<Dosen>
 }
