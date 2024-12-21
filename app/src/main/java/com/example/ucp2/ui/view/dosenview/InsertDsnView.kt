261483f915a9f4b586af02f463da39808b5d50b7
kaniadhea <kaniadhea7@gmail.com>
2024-12-21 22:03:56 +0700

    membuat column untuk menyusun komponen secara vertikal

--- app/src/main/java/com/example/ucp2/ui/view/dosenview/InsertDsnView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosenview/InsertDsnView.kt
@@ -1,5 +1,9 @@
 package com.example.ucp2.ui.view.dosenview
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.ucp2.ui.viewmodel.dosen.DosenEvent
@@ -13,5 +17,19 @@ fun FormDosen(
     errorState: FormErrorState = FormErrorState(),
     modifier: Modifier = Modifier
 ){
+    val jenisKelamin = listOf("Laki-Laki", "Perempuan")
 
+    Column (modifier= modifier.fillMaxWidth()
+    ){
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = dosenEvent.nama,
+            onValueChange = {
+                onValueChange(dosenEvent.copy(nama = it))
+            },
+            label = { Text("Nama")},
+            isError = errorState.nama != null,
+            placeholder = { Text("Masukkan nama")}
+        )
+    }
 }
