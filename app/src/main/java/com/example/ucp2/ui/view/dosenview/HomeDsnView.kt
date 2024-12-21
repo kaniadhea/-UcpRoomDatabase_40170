2e55b7b8539cf604207b7223351da779d8b5eb2f
kaniadhea <kaniadhea7@gmail.com>
2024-12-22 01:20:57 +0700

    layout untuk menampilkan elemen secara vertikal

--- app/src/main/java/com/example/ucp2/ui/view/dosenview/HomeDsnView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosenview/HomeDsnView.kt
@@ -1,7 +1,13 @@
 package com.example.ucp2.ui.view.dosenview
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.ucp2.entity.Dosen
 
 @Composable
@@ -11,5 +17,14 @@ fun CardDsn(
     modifier: Modifier = Modifier,
     onClick: () -> Unit = {}
 ) {
+    Column (
+        modifier = Modifier.padding(8.dp)
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ){
 
+        }
+    }
 }
