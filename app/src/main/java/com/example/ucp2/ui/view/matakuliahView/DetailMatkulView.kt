7c424cb1561023f85a562e84d95e58c6e10acb2e
kaniadhea <kaniadhea7@gmail.com>
2024-12-23 01:39:04 +0700

    untuk menampilkan loadiing

--- app/src/main/java/com/example/ucp2/ui/view/matakuliahView/DetailMatkulView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/matakuliahView/DetailMatkulView.kt
@@ -1,16 +1,23 @@
 package com.example.ucp2.ui.view.matakuliahView
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,6 +25,26 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2.entity.Matakuliah
+import com.example.ucp2.ui.viewmodel.matakuliah.DetailUiState
+
+@Composable
+fun BodyBetailMatkul(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState(),
+    onDeleteClick: () -> Unit = {}
+){
+    var deleteConfirmationRequest by rememberSaveable { mutableStateOf(false) }
+    when {
+        detailUiState.isLoading -> {
+            Box (
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+
+            ){
+                CircularProgressIndicator()
+            }
+        }
+}
 
 @Composable
 fun ItemDetailMatkul(
