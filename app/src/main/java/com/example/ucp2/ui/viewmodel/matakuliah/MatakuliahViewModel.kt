b540a30e01592deccf176dbb88f62f9ef78ea36b
kaniadhea <kaniadhea7@gmail.com>
2024-12-22 13:47:17 +0700

    membuat data class MatkulUIState

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/MatakuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/MatakuliahViewModel.kt
@@ -3,6 +3,23 @@ package com.example.ucp2.ui.viewmodel.matakuliah
 import com.example.ucp2.entity.Matakuliah
 
 
+
+
+data class MatkulUIState(
+    val matakuliahEvent: MatakuliahEvent,
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null,
+)
+
+data class FormErrorState(
+    val kode: String? = null,
+    val nama : String? = null,
+    val sks : String? = null,
+    val semester : String? = null,
+    val jenis : String? = null,
+    val dosenpengampu : String? = null
+)
+
 data class MatakuliahEvent(
     val kode : String,
     val nama : String,
