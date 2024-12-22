e39a92df96dbc10cde3cdcc70ab09c71e264b562
kaniadhea <kaniadhea7@gmail.com>
2024-12-22 23:36:50 +0700

    membuat row untuk menyusun secara horizontal

--- app/src/main/java/com/example/ucp2/ui/view/matakuliahView/HomeMatkulView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/matakuliahView/HomeMatkulView.kt
@@ -1,11 +1,21 @@
 package com.example.ucp2.ui.view.matakuliahView
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ucp2.entity.Matakuliah
 
 @Composable
@@ -21,6 +31,23 @@ fun  CardMatkul(
             .fillMaxWidth()
             .padding(8.dp)
     ){
+        Column (
+            modifier = Modifier.padding(8.dp)
+        ){
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = matkul.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
 
+            }
+
+        }
     }
 }
