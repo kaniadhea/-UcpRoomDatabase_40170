28ed4b593012aa84f375fd5101f9738226528059
kaniadhea <kaniadhea7@gmail.com>
2024-12-22 15:47:26 +0700

    Memantau perubahan state dari ViewModel

--- app/src/main/java/com/example/ucp2/ui/view/matakuliahView/UpdateMatkulView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/matakuliahView/UpdateMatkulView.kt
@@ -1,6 +1,10 @@
 package com.example.ucp2.ui.view.matakuliahView
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2.ui.viewmodel.PenyediaViewModel
@@ -13,5 +17,12 @@ fun UpdateMatkulView(
     modifier: Modifier = Modifier,
     viewModel: UpdateMatkulViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
+    val uiState = viewModel.updateUIState
+    val snackbarHostState = remember { SnackbarHostState () }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(uiState.snackBarMessage) {
+        println("LaunchedEffect triggerd")
+    }
 
 }
