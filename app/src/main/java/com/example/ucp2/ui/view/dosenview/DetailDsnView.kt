43bf55b3e7ebe74b5e801e56556ed32233a35de8
kaniadhea <kaniadhea7@gmail.com>
2024-12-22 13:13:45 +0700

    membuat fungsi ItemDetailDsn didalamnya tedapat card

---
+++ app/src/main/java/com/example/ucp2/ui/view/dosenview/DetailDsnView.kt
@@ -0,0 +1,69 @@
+package com.example.ucp2.ui.view.dosenview
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.view.KeyEventDispatcher.Component
+import com.example.ucp2.entity.Dosen
+
+@Composable
+fun ItemDetailDsn(
+    modifier: Modifier = Modifier,
+    dosen: Dosen
+){
+    Card (
+        modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column (
+            modifier = Modifier.padding(16.dp)
+
+        ) {
+            ComponentDetailDsn ( judul = "NIDN" , isinya = dosen.nidn)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailDsn ( judul = "Nama" , isinya = dosen.nama)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailDsn ( judul = "Jenis Kelamin" , isinya = dosen.jenisKelamin)
+        }
+    }
+}
+
+
+@Composable
+fun ComponentDetailDsn(
+    modifier: Modifier = Modifier,
+    judul : String,
+    isinya : String,
+){
+    Column (
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ){
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya, fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+
+            )
+    }
+}
