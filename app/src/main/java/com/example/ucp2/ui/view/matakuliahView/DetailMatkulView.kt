063ac46d2aa53832c48a4f3fb8c83f4bfb8b9d07
kaniadhea <kaniadhea7@gmail.com>
2024-12-23 01:32:59 +0700

    membuat card

--- app/src/main/java/com/example/ucp2/ui/view/matakuliahView/DetailMatkulView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/matakuliahView/DetailMatkulView.kt
@@ -3,6 +3,9 @@ package com.example.ucp2.ui.view.matakuliahView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -11,6 +14,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.ucp2.entity.Matakuliah
+
+@Composable
+fun ItemDetailMatkul(
+    modifier: Modifier = Modifier,
+    matakuliah: Matakuliah
+){
+    Card (
+        modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ){
+
+    }
+}
 
 
 @Composable
