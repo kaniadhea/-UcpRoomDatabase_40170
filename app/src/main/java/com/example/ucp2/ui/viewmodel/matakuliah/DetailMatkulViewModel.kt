160523f42f00153df4e3f7c7c708f8c2b9e4ab9f
kaniadhea <kaniadhea7@gmail.com>
2024-12-22 15:24:38 +0700

    untuk menangani pemuatan data loading, error, dan penghapusan data

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/DetailMatkulViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/DetailMatkulViewModel.kt
@@ -1,6 +1,73 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.entity.Matakuliah
+import com.example.ucp2.repository.RepositoryMatkul
+import com.example.ucp2.ui.navigation.DestinasiDetail
+import com.example.ucp2.ui.navigation.DestinasiDetailMatkul
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class DetailMatkulViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMatkul: RepositoryMatkul,
+) : ViewModel() {
+    private val  _kode : String = checkNotNull(savedStateHandle[DestinasiDetailMatkul.KODE])
+
+    val detailUiState : StateFlow<DetailUiState> = repositoryMatkul.getMatkul(_kode)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = true,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+
+                    )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
+    fun deleteMhs() {
+        detailUiState.value.detailUiEvent.toMatakuliahEntity().let{
+            viewModelScope.launch{
+                repositoryMatkul.deleteMatkul(it)
+            }
+        }
+    }
+
+}
+
+data class DetailUiState(
+    val detailUiEvent: MatakuliahEvent = MatakuliahEvent(),
+    val isLoading: Boolean = false,
+    val isError : Boolean = false,
+    val errorMessage: String = ""
+)
 
 
 fun Matakuliah.toDetailUiEvent() : MatakuliahEvent {
