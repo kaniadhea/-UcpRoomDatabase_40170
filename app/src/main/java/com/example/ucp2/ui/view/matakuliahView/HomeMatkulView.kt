8ea0d4e6c6fdd8296244d8fb53ced044aa6dc8c2
kaniadhea <kaniadhea7@gmail.com>
2024-12-23 00:16:07 +0700

    Menampilkan indikator loading

--- app/src/main/java/com/example/ucp2/ui/view/matakuliahView/HomeMatkulView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/matakuliahView/HomeMatkulView.kt
@@ -1,25 +1,87 @@
 package com.example.ucp2.ui.view.matakuliahView
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2.entity.Matakuliah
+import com.example.ucp2.ui.viewmodel.dosen.HomeUiState
+import kotlinx.coroutines.launch
 
 @Composable
+fun BodyHomeMatkulView(
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState () }
+    when{
+        homeUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
 
+        homeUiState.isError -> {
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ListMatakuliah(
+    listMatkul: List<Matakuliah>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+){
+    LazyColumn (
+        modifier = modifier
+    ){
+        items(
+            items = listMatkul,
+            itemContent = { matkul ->
+                CardMatkul(
+                    matkul = matkul,
+                    onClick = {onClick(matkul.kode)}
+                )
+            }
+        )
+    }
+}
+
+
+@Composable
 fun  CardMatkul(
     matkul: Matakuliah,
     modifier: Modifier = Modifier,
