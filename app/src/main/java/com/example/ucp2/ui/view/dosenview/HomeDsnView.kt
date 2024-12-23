edd617950c4eb1fc1aae1eb7b955e7b7e86d8c39
kaniadhea <kaniadhea7@gmail.com>
2024-12-23 09:52:55 +0700

    membuat fungsi home dan fungsi BodyHomeDsnView

--- app/src/main/java/com/example/ucp2/ui/view/dosenview/HomeDsnView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosenview/HomeDsnView.kt
@@ -1,26 +1,126 @@
 package com.example.ucp2.ui.view.dosenview
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2.entity.Dosen
+import com.example.ucp2.ui.viewmodel.PenyediaViewModel
+import com.example.ucp2.ui.viewmodel.dosen.HomeDsnViewModel
+import com.example.ucp2.ui.viewmodel.dosen.HomeUiState
+import kotlinx.coroutines.launch
+
 
 @Composable
+fun HomeMhsView(
+    viewModel: HomeDsnViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddDsn: () -> Unit = {},
+    onDetailClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+){
 
+}
+
+@Composable
+fun BodyHomeDsnView(
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when{
+        homeUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+        homeUiState.isError -> {
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message) //Tampilkan Snackbar
+                    }
+                }
+            }
+        }
+        homeUiState.listDsn.isEmpty() -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+
+            ){
+                Text(
+                    text = "Tidak ada data mahasiswa",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+        else -> {
+            //Menampilkan daftar mahasiswa
+            ListDosen(
+                listDsn = homeUiState.listDsn,
+                onClick = {
+                    onClick (it)
+                    println(it
+                    )
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
+
+@Composable
+fun ListDosen(
+    listDsn: List<Dosen>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+) {
+    LazyColumn (
+        modifier = modifier
+    ) {
+        items(
+            items = listDsn,
+            itemContent = { dsn ->
+                CardDsn(
+                    dsn = dsn,
+                    onClick = {onClick(dsn.nidn)}
+                )
+            }
+        )
+    }
+}
+
+@Composable
 fun CardDsn(
     dsn: Dosen,
     modifier: Modifier = Modifier,
